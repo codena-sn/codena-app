101c76264e844680accdabe127cf9d504d9c0e41
codena-sn <danoption1@gmail.com>
2026-04-01 23:46:04 +0200

    Remove ReactDOM and routing setup

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,16 +1,2 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login.jsx'
-import Dashboard from './Dashboard.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/cms" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-)
+import React
