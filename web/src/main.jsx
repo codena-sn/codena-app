5e96ee67bab02c5e6962d680e30abb807495142d
codena-sn <danoption1@gmail.com>
2026-04-01 23:48:22 +0200

    Import ReactDOM and create App component
    Added ReactDOM import and a basic App component.

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,2 +1,8 @@
 import React from 'react'
-import React
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <div className="bg
