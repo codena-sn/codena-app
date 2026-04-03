7d844fdfdb97b9919783b65d4e7f5473fe40bb8d
codena-sn <danoption1@gmail.com>
2026-04-04 01:25:05 +0200

    Wrap App component with BrowserRouter

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-function App() {
-  return (
-    <div style={{ fontFamily: "system-ui", padding: 24 }}>
-      <h1>Codéna</h1>
-      <p>Build OK ✅</p>
-      <p>
-        API Docs :{" "}
-        <a href="https://codena-backend.onrender.com/docs" target="_blank" rel="noreferrer">
-          Swagger
-        </a>
-      </p>
-    </div>
-  );
-}
-
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
