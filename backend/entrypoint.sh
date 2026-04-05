a3af6769db532ad080d6ad05cdf7473984595182
codena-sn <danoption1@gmail.com>
2026-04-05 19:10:58 +0200

    Fix uvicorn port
    Update entrypoint to use dynamic port from environment variable.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env sh
-set -e
-python -m app.wait_for_db
-alembic upgrade head
-python -m app.seed
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
